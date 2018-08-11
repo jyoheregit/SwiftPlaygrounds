ea2a11252015b270cfbf558f0097204eb8693dc5
Joe <jyohere.d@gmail.com>
2018-08-11 18:29:55 +0530

    Second Playground - Arrays and Dictionaries first commit

---
+++ SecondPlayground.playground/Contents.swift
@@ -0,0 +1,27 @@
+//Arrays and Dictionaries
+
+var stringArray : [String]
+
+stringArray = ["ABC", "DEF"]
+stringArray += ["HIJ"]
+
+var dictionary : [String : String]
+
+dictionary = ["A" : "ABC", "B" : "BCD"]
+dictionary["A"]
+dictionary["B"]
+
+let intArray = [1,2,3,4]
+intArray.count
+
+let anotherDictionary = [1 : "One", 2 : "Two", 3 : "Three"]
+anotherDictionary[1]
+anotherDictionary[2]
+
+let intDictionary = [1 : 1, 2: 2]
+intDictionary[1]
+intDictionary[2]
+
+let mixDictionary = [1: true, 2: false]
+mixDictionary[1]
+mixDictionary[2]
