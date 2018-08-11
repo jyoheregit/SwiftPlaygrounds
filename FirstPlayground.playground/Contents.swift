012cdbda506a34acbaca1895a9d9160dc3202f80
Joe <jyohere.d@gmail.com>
2018-08-11 17:26:48 +0530

    First commit

---
+++ FirstPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+let constant = "Constant"
+var variable = "Variable"
+
+//constant = "NewConstant" - Immutable
+
+//Data types
+var integer : Int = 10
+var float : Float = 10.4
+var double : Double = 100.4
+var boolean : Bool = false
+var string : String = "string"
+var char : Character = "C"
+
+let newFloat = Float(integer) + float
+let newInt =  integer + Int(float)
+let newString = string + " new"
+let anotherString = String(char) + string
+
+
